e3b7047872487e58a13ecc9ad4e1644b2887d20c
Dan "Deebster" Smith <29483+Deebster@users.noreply.github.com>
2021-10-22 16:08:01 +0700

    Set up linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,22 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
+	ignorePatterns: ['*.cjs', 'svelte.config.js'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2019
+		ecmaVersion: 2019,
+		project: ["tsconfig.json"],
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'@typescript-eslint/switch-exhaustiveness-check': 'error',
 	}
 };
